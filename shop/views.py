ff0a7eca55394cb057929a39b4beda59ee06bea3
vimalwyn <vimalwynad@live.com>
2021-08-12 23:52:52 +0530

    commit

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ def AllprodCat(request,c_slug=None):
         products_list = Product.objects.all().filter(category=c_page,available=True)
     else:
         products_list = Product.objects.all().filter(available=True)
-    paginator = Paginator(products_list,8)
+    paginator = Paginator(products_list,6)
     try:
         page = int(request.GET.get('page','1'))
     except:
